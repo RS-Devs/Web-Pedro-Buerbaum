691133b52cee35e3a177bbb976c623f0e3159135
RS-DEVS <pedrodeveloper98@gmail.com>
2023-06-28 00:59:25 +0200

    Actualizado4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import MainPage from "./MainPage";
-import ProjectsSection from "./ProjectSection";
-import SocialMediaSection from "./SocialMediaSection";
-import ContactSection from "./ContactSection";
+import  { useState, lazy, Suspense } from "react";
 import "animate.css";
 
+const MainPage = lazy(() => import("./MainPage"));
+const ProjectsSection = lazy(() => import("./ProjectSection"));
+const SocialMediaSection = lazy(() => import("./SocialMediaSection"));
+const ContactSection = lazy(() => import("./ContactSection"));
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState("main");
@@ -12,7 +12,7 @@ const App = () => {
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
-    setShowButtons(false); 
+    setShowButtons(false);
   };
 
   const handleMenuToggle = () => {
@@ -81,15 +81,20 @@ const App = () => {
         </div>
       </div>
 
-      {currentPage === "main" && (
-        <MainPage className="absolute top-0 left-0 w-full h-full z-0" />
-      )}
-      {currentPage === "projects" && <ProjectsSection />}
-      {currentPage === "socialMedia" && <SocialMediaSection />}
-      {currentPage === "contact" && <ContactSection />}
-
+      <Suspense fallback={
+  <div className="flex items-center justify-center h-screen bg-black">
+    
+  </div>
+}>
+        {currentPage === "main" && (
+          <MainPage className="absolute top-0 left-0 w-full h-full z-0" />
+        )}
+        {currentPage === "projects" && <ProjectsSection />}
+        {currentPage === "socialMedia" && <SocialMediaSection />}
+        {currentPage === "contact" && <ContactSection />}
+      </Suspense>
     </div>
   );
 };
 
-export default App; 
+export default App;
